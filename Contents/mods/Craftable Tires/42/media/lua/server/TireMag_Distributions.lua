1eb37471be3de7c1fecaf95ac47f4f761e1d4ca0
Luis Alejandro <aluis@frba.utn.edu.ar>
2026-09-04 01:32:36 -0300

    small optimization to reduce global object calls

--- Contents/mods/Craftable Tires/42/media/lua/server/TireMag_Distributions.lua
+++ Contents/mods/Craftable Tires/42/media/lua/server/TireMag_Distributions.lua
@@ -1,39 +1,43 @@
 require 'Items/ProceduralDistributions'
 require 'Items/Distributions'
 
+local procedural = ProceduralDistributions
+
 -----------------------------------------Magazine-----------------------------------------
 ---MagazineRackMixed---
-table.insert(ProceduralDistributions["list"]["MagazineRackMixed"].items, "DP.TireMag")
-table.insert(ProceduralDistributions["list"]["MagazineRackMixed"].items, 0.025)
+table.insert(procedural["list"]["MagazineRackMixed"].items, "DP.TireMag")
+table.insert(procedural["list"]["MagazineRackMixed"].items, 0.025)
 ---LibraryBooks---
-table.insert(ProceduralDistributions["list"]["LibraryBooks"].items, "DP.TireMag")
-table.insert(ProceduralDistributions["list"]["LibraryBooks"].items, 0.025)
+table.insert(procedural["list"]["LibraryBooks"].items, "DP.TireMag")
+table.insert(procedural["list"]["LibraryBooks"].items, 0.025)
 ---Living Room SideTable---
-table.insert(ProceduralDistributions["list"]["LivingRoomSideTableNoRemote"].items, "DP.TireMag")
-table.insert(ProceduralDistributions["list"]["LivingRoomSideTableNoRemote"].items, 0.05)
+table.insert(procedural["list"]["LivingRoomSideTableNoRemote"].items, "DP.TireMag")
+table.insert(procedural["list"]["LivingRoomSideTableNoRemote"].items, 0.05)
 ---LivingRoomSideTable---
-table.insert(ProceduralDistributions["list"]["LivingRoomSideTable"].items, "DP.TireMag")
-table.insert(ProceduralDistributions["list"]["LivingRoomSideTable"].items, 0.05)
+table.insert(procedural["list"]["LivingRoomSideTable"].items, "DP.TireMag")
+table.insert(procedural["list"]["LivingRoomSideTable"].items, 0.05)
 ---Garage Metalwork---
-table.insert(ProceduralDistributions["list"]["GarageMetalwork"].items, "DP.TireMag")
-table.insert(ProceduralDistributions["list"]["GarageMetalwork"].items, 0.1)
+table.insert(procedural["list"]["GarageMetalwork"].items, "DP.TireMag")
+table.insert(procedural["list"]["GarageMetalwork"].items, 0.1)
 ---Garage Mechanics---
-table.insert(ProceduralDistributions["list"]["GarageMechanics"].items, "DP.TireMag")
-table.insert(ProceduralDistributions["list"]["GarageMechanics"].items, 1.0)
+table.insert(procedural["list"]["GarageMechanics"].items, "DP.TireMag")
+table.insert(procedural["list"]["GarageMechanics"].items, 1.0)
 ---Mechanic Shelf Books---
-table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, "DP.TireMag")
-table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, 20)
+table.insert(procedural["list"]["MechanicShelfBooks"].items, "DP.TireMag")
+table.insert(procedural["list"]["MechanicShelfBooks"].items, 20)
 ---Magazine crate---
-table.insert(ProceduralDistributions["list"]["CrateMagazines"].items, "DP.TireMag")
-table.insert(ProceduralDistributions["list"]["CrateMagazines"].items, 0.05)
+table.insert(procedural["list"]["CrateMagazines"].items, "DP.TireMag")
+table.insert(procedural["list"]["CrateMagazines"].items, 0.05)
 
 ---Zombie Drops---
 Distributions = Distributions or {}
 
-SuburbsDistributions.all.Outfit_Mechanic = SuburbsDistributions.all.Outfit_Mechanic or {rolls = 1,items = {},junk= {rolls =1, items={}}}
-table.insert(SuburbsDistributions["all"]["Outfit_Mechanic"].items, "DP.TireMag")
-table.insert(SuburbsDistributions["all"]["Outfit_Mechanic"].items, 1)
+local suburbs = SuburbsDistributions
 
-SuburbsDistributions.all.Outfit_MetalWorker = SuburbsDistributions.all.Outfit_MetalWorker or {rolls = 1,items = {},junk= {rolls =1, items={}}}
-table.insert(SuburbsDistributions["all"]["Outfit_MetalWorker"].items, "DP.TireMag")
-table.insert(SuburbsDistributions["all"]["Outfit_MetalWorker"].items, 1)
+suburbs.all.Outfit_Mechanic = suburbs.all.Outfit_Mechanic or {rolls = 1,items = {},junk= {rolls =1, items={}}}
+table.insert(suburbs["all"]["Outfit_Mechanic"].items, "DP.TireMag")
+table.insert(suburbs["all"]["Outfit_Mechanic"].items, 1)
+
+suburbs.all.Outfit_MetalWorker = suburbs.all.Outfit_MetalWorker or {rolls = 1,items = {},junk= {rolls =1, items={}}}
+table.insert(suburbs["all"]["Outfit_MetalWorker"].items, "DP.TireMag")
+table.insert(suburbs["all"]["Outfit_MetalWorker"].items, 1)
